ce92e2e8b591d697d6a6b4d78ac2898836e4dfb9
James Cowlishaw <james@cowlibob.co.uk>
2010-12-18 01:11:08 +0000

    Fixed rspec it() string for Builder::Configuration type.

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,7 +2,7 @@ require 'helper'
 include NSIS
 
 describe "NSIS::Config" do
-  it "should return an object of type NSIS::Config" do
+  it "should return an object of type NSIS::Configuration" do
     Builder.config{}.should be_an_instance_of(Builder::Configuration)
   end
   
