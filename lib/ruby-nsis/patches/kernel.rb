a2a4b52c7847cc7bb514f6660d5a7fe678f98cfa
James Cowlishaw <james@cowlibob.co.uk>
2010-12-21 22:56:24 +0000

    Silenced Kernel::require_glob; enough of your chat, already.

--- lib/ruby-nsis/patches/kernel.rb
+++ lib/ruby-nsis/patches/kernel.rb
@@ -3,8 +3,7 @@ module Kernel
     absolute_path = File.join(File.dirname(caller[0]), path.to_s)
     if path.match(/[\*\?]/)
       Dir.glob(absolute_path).each do |rb|
-        puts "require_relative(#{rb})"
-        puts require rb
+        require rb
       end
     else
       require absolute_path
